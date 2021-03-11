a94cdea0eceb47fdc0f9591b29d936f79277ac5d
coolmanben <mZmiterizrigi@gmail.com>
2021-03-11 15:16:09 +0300

    fix: work with matrix
    fix: work with matrix

--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  var newarray   = new Array();
+module.exports = function towelSort (matrix) { 
   var resarray   = new Array();
-  var temparray  = new Array();  
-  var q = 0 ;
-  var sumel = 0;
-  var sizearray = 0;
-  for (var i = 0; i < newarray.length ; i++ ) {   
-    temparray = newarray[i];
-    if (sizearray == 0) {sizearray = (newarray.length + 1) *  (newarray.length + 1);}
-    if (q == 0) {
-      for ( ; q < temparray.length ; q++ ) {                   
-        resarray.push( temparray[q] ) ;
-        sumel++;
-      } 
+  for (var i = 0; i < matrix.length ; i++ ) {        
+    if (i % 2) {              
+      resarray.push( matrix[i].reverse() ) ;       
     }
     else {
-      q = temparray.length - 1;
-      for (  ; q >= 0 ; q-- ) {                   
-        resarray.push( temparray[q] ) ;
-        sumel++;
-      }
-      q++; 
+      resarray.push( matrix[i] ) ;
     }     
   }
-  if ( sumel != sizearray ) { resarray.length = resarray.length + 1; }
-  return resarray; 
+  return resarray.flat(); 
 }
