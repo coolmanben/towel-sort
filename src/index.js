746969c170b16d28d446ae5ea968ea3666c6ee4b
coolmanben <mZmiterizrigi@gmail.com>
2021-03-11 14:06:32 +0300

    fix: fixed for non-square arrays
    fix: fixed for non-square arrays

--- src/index.js
+++ src/index.js
@@ -5,21 +5,27 @@ module.exports = function towelSort (matrix) {
   var newarray   = new Array();
   var resarray   = new Array();
   var temparray  = new Array();  
-  var q = 0 ; 
+  var q = 0 ;
+  var sumel = 0;
+  var sizearray = 0;
   for (var i = 0; i < newarray.length ; i++ ) {   
     temparray = newarray[i];
     if (q == 0) {
       for ( ; q < temparray.length ; q++ ) {                   
-        resarray.push( temparray[q] ) ;  
+        resarray.push( temparray[q] ) ;
+        sumel++;
+        if (sizearray == 0) { izearray = newarray.length *  temparray.length }  
       } 
     }
     else {
       q = temparray.length - 1;
       for (  ; q >= 0 ; q-- ) {                   
-          resarray.push( temparray[q] ) ;  
+        resarray.push( temparray[q] ) ;
+        sumel++;
       }
       q++; 
     }     
   }
+  if ( sumel != sizearray ){ resarray.length = sizearray; }
   return resarray; 
 }
