66cdd11249d0f7a9c1a3435994dfae98a587d696
coolmanben <mZmiterizrigi@gmail.com>
2021-03-11 13:05:52 +0300

    fix: remade in accordance with the assignment
    fix: remade in accordance with the assignment

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@ module.exports = function towelSort (matrix) {
   var newarray   = new Array();
   var resarray   = new Array();
   var temparray  = new Array();  
+  var q = 0 ; 
   for (var i = 0; i < newarray.length ; i++ ) {   
     temparray = newarray[i];
-      for (var q = 0; q < temparray.length ; q++ ) {                   
-        resarray.push( temparray[q] ) ;          
+    if (q == 0) {
+      for ( ; q < temparray.length ; q++ ) {                   
+        resarray.push( temparray[q] ) ;  
       } 
+    }
+    else {
+      q--;
+      for (  ; q > 0 ; q-- ) {                   
+          resarray.push( temparray[q] ) ;  
+      } 
+    }     
   }
   return resarray; 
 }
